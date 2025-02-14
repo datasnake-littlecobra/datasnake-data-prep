4c01544ae171ac9b0b0526c1b0339d8b6c5289b3
datasnake-littlecobra <prabhakar.sharma@datasnake.io>
2025-02-13 21:36:58 -0600

    loading gadm data to cassandra will check cache data too

--- save_gadm_to_cassandra.py
+++ save_gadm_to_cassandra.py
@@ -19,49 +19,44 @@ def save_to_cassandra_main(df, cluster_ips, keyspace, gadm_level):
         logging.info(keyspace)
         print(cluster_ips.split(","))
         print(keyspace)
-        # session = connect_cassandra(keyspace)
-        USERNAME = "cassandra"
-        PASSWORD = "cassandra"
-        CASSANDRA_HOSTS = ["127.0.0.1"]
-        auth_provider = PlainTextAuthProvider(USERNAME, PASSWORD)
-        cluster = Cluster(CASSANDRA_HOSTS, auth_provider=auth_provider)
-        session = cluster.connect()
-        session.set_keyspace("test_keyspace")
-        print("cassandra connection established!")
+        session = connect_cassandra(keyspace)
+        # USERNAME = "cassandra"
+        # PASSWORD = "cassandra"
+        # CASSANDRA_HOSTS = ["127.0.0.1"]
+        # auth_provider = PlainTextAuthProvider(USERNAME, PASSWORD)
+        # cluster = Cluster(CASSANDRA_HOSTS, auth_provider=auth_provider)
+        # session = cluster.connect()
+        # session.set_keyspace("test_keyspace")
+        # print("cassandra connection established!")
         # dataframe = pl.DataFrame(
         #     {"stock_id": [uuid4() for _ in range(3)]},
         #     {"symbol": ["AAPL", "MSFT", "GOOG"]},
         #     {"price": [140, 134, 142]},
         #     {"timestamp": [datetime.datetime.now() for _ in range(3)]},
         # )
-        print("dataframe is ready!")
+        # print("dataframe is ready!")
         # print(dataframe.head())
-        print("inserting into stocks table")
-        insertquery = "INSERT INTO stocks (stock_id, symbol, price, timestamp) VALUES (uuid(),'AAPL',140,toTimestamp(now())) IF NOT EXISTS"
+        # print("inserting into stocks table")
+        # insertquery = "INSERT INTO stocks (stock_id, symbol, price, timestamp) VALUES (uuid(),'AAPL',140,toTimestamp(now())) IF NOT EXISTS"
         # data = [
         #     (row["stock_id"], row["symbol"], row["price"], row["timestamp"])
         #     for row in dataframe.to_dicts()
         # ]
         # for row in data:
-        print(insertquery)
-        session.execute(insertquery)
-        print("data inserted successfully")
+        # print(insertquery)
+        # session.execute(insertquery)
+        # print("data inserted successfully")
         # insert_sample_data(session)
         # optimized_batch_insert_cassandra(
         #     session, keyspace, gadm_level, df, batch_size=50, sleep_time=0.1
         # )
         # batch_insert_cassandra(session, table_name, dataframe, batch_size, timeout)
-        # batch_insert_cassandra_async(session, keyspace, gadm_level, df, concurrency=10)
+        batch_insert_cassandra_async(session, keyspace, gadm_level, df, concurrency=10)
         # optimized_insert_cassandra(
         #     session, keyspace, gadm_level, df, concurrency=5, batch_size=100
         # )
     except Exception as e:
         logging.error(f"Error writing to Cassandra: {e}")
-    finally:
-        if session is not None:  # Check if session was successfully created
-            logging.info("Closing Cassandra session...")
-            # session.shutdown()
-            raise
 
 
 def insert_sample_data(session):
